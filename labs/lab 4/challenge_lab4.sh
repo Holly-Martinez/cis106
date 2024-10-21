b66ef692e9ae02c46574bc1d7769e991a0ef389f
Holly-Martinez <hrmartinez@students.pccc.edu>
2024-10-20 22:30:20 -0400

    Lab 4 Done

---
+++ labs/lab 4/challenge_lab4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Changing to:  $HOME/Pictures"
+echo "Changing to:  $HOME/Videos"
+echo "Current PWD:  $HOME/Pictures"
+echo "Previous PWD: $HOME/Videos"
+echo " "
+echo "Long list of:  $HOME/cis106"
+ls -olhg ~/cis106
+
+
---
+++ labs/lab 4/lab4.md
@@ -0,0 +1,12 @@
+# Lab 4
+
+## Question 1
+![q1](q1.png)
+## Question 2
+![q2](q2.png)
+## Question 3
+![q3](q3.png)
+## Question 4 (Challange Question)
+![Script Source Code](challenge_lab4.sh)
+![lab 4 chg](challenge_q.png)
+
